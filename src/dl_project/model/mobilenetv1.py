a293c31671df2a2c97122b9c26117149d233b054
cristiano2003 <nguyentrungtruc2003@gmail.com>
2023-12-07 15:23:32 +0700

    update mobilenetv1

--- src/dl_project/model/mobilenetv1.py
+++ src/dl_project/model/mobilenetv1.py
@@ -1,122 +1,56 @@
-from typing import Callable, Any, Optional
-
-import torch
-from torch import Tensor
-from torch import nn
-from torchvision.ops.misc import Conv2dNormActivation
-
+import torch.nn as nn
+from torchsummary import summary
 
 class MobileNetV1(nn.Module):
-
-    def __init__(
-            self,
-            num_classes: int = 36,
-    ) -> None:
+    def __init__(self, ch_in = 3, n_classes = 36):
         super(MobileNetV1, self).__init__()
-        self.features = nn.Sequential(
-            Conv2dNormActivation(3,
-                                 32,
-                                 kernel_size=3,
-                                 stride=2,
-                                 padding=1,
-                                 norm_layer=nn.BatchNorm2d,
-                                 activation_layer=nn.ReLU,
-                                 inplace=True,
-                                 bias=False,
-                                 ),
 
-            DepthWiseSeparableConv2d(32, 64, 1),
-            DepthWiseSeparableConv2d(64, 128, 2),
-            DepthWiseSeparableConv2d(128, 128, 1),
-            DepthWiseSeparableConv2d(128, 256, 2),
-            DepthWiseSeparableConv2d(256, 256, 1),
-            DepthWiseSeparableConv2d(256, 512, 2),
-            DepthWiseSeparableConv2d(512, 512, 1),
-            DepthWiseSeparableConv2d(512, 512, 1),
-            DepthWiseSeparableConv2d(512, 512, 1),
-            DepthWiseSeparableConv2d(512, 512, 1),
-            DepthWiseSeparableConv2d(512, 512, 1),
-            DepthWiseSeparableConv2d(512, 1024, 2),
-            DepthWiseSeparableConv2d(1024, 1024, 1),
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+                )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                # dw
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+
+                # pw
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+                )
+
+        self.model = nn.Sequential(
+            conv_bn(ch_in, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AdaptiveAvgPool2d(1)
         )
+        self.fc = nn.Linear(1024, n_classes)
 
-        self.avgpool = nn.AvgPool2d((7, 7))
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        return x
 
-        self.classifier = nn.Linear(1024, num_classes)
-
-        # Initialize neural network weights
-        self._initialize_weights()
-
-    def forward(self, x: Tensor) -> Tensor:
-        out = self._forward_impl(x)
-
-        return out
-
-    # Support torch.script function
-    def _forward_impl(self, x: Tensor) -> Tensor:
-        out = self.features(x)
-        out = self.avgpool(out)
-        out = torch.flatten(out, 1)
-        out = self.classifier(out)
-
-        return out
-
-    def _initialize_weights(self) -> None:
-        for module in self.modules():
-            if isinstance(module, nn.Conv2d):
-                nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
-                if module.bias is not None:
-                    nn.init.zeros_(module.bias)
-            elif isinstance(module, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.ones_(module.weight)
-                nn.init.zeros_(module.bias)
-            elif isinstance(module, nn.Linear):
-                nn.init.normal_(module.weight, 0, 0.01)
-                nn.init.zeros_(module.bias)
-
-
-class DepthWiseSeparableConv2d(nn.Module):
-    def __init__(
-            self,
-            in_channels: int,
-            out_channels: int,
-            stride: int,
-            norm_layer: Optional[Callable[..., nn.Module]] = None
-    ) -> None:
-        super(DepthWiseSeparableConv2d, self).__init__()
-        self.stride = stride
-        if stride not in [1, 2]:
-            raise ValueError(f"stride should be 1 or 2 instead of {stride}")
-
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-
-        self.conv = nn.Sequential(
-            Conv2dNormActivation(in_channels,
-                                 in_channels,
-                                 kernel_size=3,
-                                 stride=stride,
-                                 padding=1,
-                                 groups=in_channels,
-                                 norm_layer=norm_layer,
-                                 activation_layer=nn.ReLU,
-                                 inplace=True,
-                                 bias=False,
-                                 ),
-            Conv2dNormActivation(in_channels,
-                                 out_channels,
-                                 kernel_size=1,
-                                 stride=1,
-                                 padding=0,
-                                 norm_layer=norm_layer,
-                                 activation_layer=nn.ReLU,
-                                 inplace=True,
-                                 bias=False,
-                                 ),
-
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        out = self.conv(x)
-
-        return out
+if __name__=='__main__':
+    # model check
+    model = MobileNetV1(ch_in=3, n_classes=1000)
+    summary(model, input_size=(3, 224, 224), device='cpu')
